a7e0566c51355460f99af06aa795fa22ab6a3a02
Istvan Balogh <istvan.balogh@lhsystems.com>
2014-03-08 12:46:46 +0100

    change scene

---
+++ BlackMagic/BlackMagic/BMSceneFight.h
@@ -0,0 +1,13 @@
+//
+//  BMSceneFight.h
+//  BlackMagic
+//
+//  Created by Istvan Balogh on 08/03/14.
+//  Copyright (c) 2014 BlackBone. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BMSceneFight : SKScene
+
+@end
---
+++ BlackMagic/BlackMagic/BMSceneFight.m
@@ -0,0 +1,13 @@
+//
+//  BMSceneFight.m
+//  BlackMagic
+//
+//  Created by Istvan Balogh on 08/03/14.
+//  Copyright (c) 2014 BlackBone. All rights reserved.
+//
+
+#import "BMSceneFight.h"
+
+@implementation BMSceneFight
+
+@end
