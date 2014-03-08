47a4014b9ff57c99a2a9ba2f31f47fd638faf475
Istvan Balogh <istvan.balogh@lhsystems.com>
2014-03-09 00:55:48 +0100

    add png files

--- BlackMagic/BlackMagic/BMSceneFight.m
+++ BlackMagic/BlackMagic/BMSceneFight.m
@@ -27,12 +27,11 @@
     
     
     
-    NSArray* playerMinions;
-    NSArray* opponenetMinions;
+    NSMutableArray* playerCardSprites;
+    NSMutableArray* opponenetCardSprites;
     NSArray* playerCardPositions;
     NSArray* opponenetCardPositions;
     
-    NSArray* playerAvailableCardPositions;
     //SKSpriteNode *opponentMana;
     
     BOOL _touchingCard;
@@ -65,12 +64,35 @@ NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
     return randomString;
 }
 
+-(void)addBackground{
+    
+    SKSpriteNode* card00 = [SKSpriteNode spriteNodeWithImageNamed:@"FieldW"];
+    card00.size = CGSizeMake(self.frame.size.width, self.frame.size.height / 2 +60);
+    //card00.anchorPoint = CGPointZero;
+    card00.position = CGPointMake(380, 800);
+    //card00.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:card00.size];
+    [self addChild:card00];
+    
+    SKSpriteNode* card01 = [SKSpriteNode spriteNodeWithImageNamed:@"Fieldb"];
+    card01.size = CGSizeMake(self.frame.size.width, self.frame.size.height / 2 +60);
+    //card00.anchorPoint = CGPointZero;
+    card01.position = CGPointMake(380, 200);
+    //card00.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:card00.size];
+    //[self addChild:card01];
+    
+}
+
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
         isMyTurn = NO;
         //reset the server
+        
+        [self addBackground];
+        
+        playerCardSprites = [NSMutableArray array];
+        opponenetCardSprites = [NSMutableArray array];
         
         NSString* name = [self genRandStringLength:6];
         
@@ -91,32 +113,53 @@ NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
         fightPosition = self.frame.size.height / 2;
         
         playerCardPositions = @[[NSValue valueWithCGPoint:(CGPoint){ 200, fightPosition - 50}],
-                                [NSValue valueWithCGPoint:(CGPoint){ 250, fightPosition - 50}],
-                                [NSValue valueWithCGPoint:(CGPoint){ 300, fightPosition - 50}],
+                                [NSValue valueWithCGPoint:(CGPoint){ 275, fightPosition - 50}],
                                 [NSValue valueWithCGPoint:(CGPoint){ 350, fightPosition - 50}],
-                                [NSValue valueWithCGPoint:(CGPoint){ 400, fightPosition - 50}],
-                                [NSValue valueWithCGPoint:(CGPoint){ 450, fightPosition - 50}]];
+                                [NSValue valueWithCGPoint:(CGPoint){ 425, fightPosition - 50}],
+                                [NSValue valueWithCGPoint:(CGPoint){ 500, fightPosition - 50}],
+                                [NSValue valueWithCGPoint:(CGPoint){ 575, fightPosition - 50}]];
         
         opponenetCardPositions = @[[NSValue valueWithCGPoint:(CGPoint){ 200, fightPosition }],
-                                [NSValue valueWithCGPoint:(CGPoint){ 250, fightPosition }],
-                                [NSValue valueWithCGPoint:(CGPoint){ 300, fightPosition }],
+                                [NSValue valueWithCGPoint:(CGPoint){ 275, fightPosition }],
                                 [NSValue valueWithCGPoint:(CGPoint){ 350, fightPosition }],
-                                [NSValue valueWithCGPoint:(CGPoint){ 400, fightPosition }],
-                                [NSValue valueWithCGPoint:(CGPoint){ 450, fightPosition }]];
+                                [NSValue valueWithCGPoint:(CGPoint){ 425, fightPosition }],
+                                [NSValue valueWithCGPoint:(CGPoint){ 500, fightPosition }],
+                                [NSValue valueWithCGPoint:(CGPoint){ 575, fightPosition }]];
         
-        playerAvailableCardPositions = @[[NSValue valueWithCGPoint:(CGPoint){ 400, 250 }],
-                                   [NSValue valueWithCGPoint:(CGPoint){ 450, 250 }],
-                                   [NSValue valueWithCGPoint:(CGPoint){ 500, 250 }],
-                                   [NSValue valueWithCGPoint:(CGPoint){ 550, 250 }],
-                                   [NSValue valueWithCGPoint:(CGPoint){ 600, 250 }],
-                                   [NSValue valueWithCGPoint:(CGPoint){ 650, 250 }]];
-        
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:0 green:0 blue:0 alpha:1.0];
         
         
-        [self addSpritesWithName:@"playerCard" FromArray:playerCardPositions withSize:CGSizeMake(50, 50)];
-        [self addSpritesWithName:@"opponenetCard"FromArray:opponenetCardPositions withSize:CGSizeMake(50, 50)];
-        [self addSpritesWithName:@"playerAvailableCard" FromArray:playerAvailableCardPositions withSize:CGSizeMake(50, 50)];
+//        [self addSpritesWithName:@"playerCard" FromArray:playerCardPositions withSize:CGSizeMake(50, 50)];
+        
+        int num = 0;
+        for (NSValue* value in playerCardPositions) {
+            
+            SKSpriteNode* card00 = [SKSpriteNode spriteNodeWithImageNamed:@"FieldW"];
+            card00.name = [NSString stringWithFormat:@"%@%d", @"playerCard", num];
+            card00.size = CGSizeMake(50, 50);
+            card00.anchorPoint = CGPointZero;
+            card00.position = value.CGPointValue;
+            //card00.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:card00.size];
+            [self addChild:card00];
+            [playerCardSprites addObject:card00];
+            num++;
+        }
+        
+        //[self addSpritesWithName:@"opponenetCard"FromArray:opponenetCardPositions withSize:CGSizeMake(50, 50)];
+        
+        num = 0;
+        for (NSValue* value in opponenetCardPositions) {
+            
+            SKSpriteNode* card00 = [SKSpriteNode spriteNodeWithImageNamed:@"FieldB"];
+            card00.name = [NSString stringWithFormat:@"%@%d", @"opponenetCard", num];
+            card00.size = CGSizeMake(50, 50);
+            card00.anchorPoint = CGPointZero;
+            card00.position = value.CGPointValue;
+            //card00.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:card00.size];
+            [self addChild:card00];
+            [opponenetCardSprites addObject:card00];
+            num++;
+        }
         
         
         SKSpriteNode* healthBar = [SKSpriteNode spriteNodeWithImageNamed:@"Healthbar"];
@@ -329,6 +372,9 @@ NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
             }
             NSLog(@"számolok");
             BMMIResult* miRes = [[BMMIManager sharedManager] suggestedCardForPlayer:player withEnemy:enemy inTurn:gameState.turnCount];
+            
+            SKSpriteNode* node = playerCardSprites[miRes.slotIndex];
+            node.texture = [SKTexture textureWithImageNamed:@"Spaceship"];
 
             NSDictionary* nextStep = [self stepInputTypeOfMIResult:miRes];
         
@@ -346,6 +392,7 @@ NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345
                 playerHealth.text = [NSString stringWithFormat:@"%i", player.health];
                 opponentHealth.text = [NSString stringWithFormat:@"%i", enemy.health];
                 isMyTurn = YES;
+                
             } failure:^(NSError *error) {
                 UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"ERROR"
                                                                 message:error.domain
